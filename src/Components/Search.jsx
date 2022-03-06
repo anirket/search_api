bb8632c4f6cd66d16ffd2345ab1750eb3a16b586
Aniket Kulkarni <anirajkul01@gmail.com>
2022-03-06 14:30:26 +0530

    input UI update

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -3,6 +3,7 @@ import { FiSearch } from "react-icons/fi";
 import { nowplayingmovies, switchtab, getpopularmovies, getratedmovies, getsearchedquery } from '../Redux/actions';
 import { useDispatch, useSelector } from 'react-redux';
 import _debounce from 'lodash/debounce';
+import { IoCloseSharp } from "react-icons/io5"
 
 const Search = () => {
 
@@ -65,13 +66,17 @@ const Search = () => {
 
   const switchchangeUI = (switchid) => {
 
-    if(switchid !== 4){
-      inputsearchref.current.value = "";
+    if (switchid !== 4) {
+      clearinput();
       setinputsearch("");
     }
 
     setswitch_task(Number(switchid))
     dispatch(switchtab(switchid))
+  }
+
+  const clearinput = () => {
+    inputsearchref.current.value = "";
   }
 
   const deb = useCallback(
@@ -107,7 +112,7 @@ const Search = () => {
       </div>
       <div className='Searchinput  flex items-center justify-center mt-7 border-gray-600 p-3 mx-5 md:mt-0  md:p-0 relative'>
         <div className='flex items-center'>
-          <FiSearch className='w-10 text-gray-600 font-semibold' /> <input ref={inputsearchref} onBlur={() => setshowinfomessage(false)} onChange={(event) => changeserachinput(event.target.value)} className='p-1 outline-none w-72' maxLength={20} type="text" placeholder={`Try "Singham" or "Shutter Island"`} />
+          <FiSearch className='w-10 text-gray-600 font-semibold' /> <input ref={inputsearchref} onBlur={() => setshowinfomessage(false)} onChange={(event) => changeserachinput(event.target.value)} className='p-1 outline-none w-72' maxLength={20} type="text" placeholder={`Try "Singham" or "Shutter Island"`} /> <span ><IoCloseSharp onClick={clearinput} className='text-xl' /></span>
         </div>
 
         <div className={`${showinfomessage ? "block" : "hidden"}   Infomessage absolute top-14 w-80 bg-white p-3 rounded-lg z-50 mt-2 md:mt-0`}>
